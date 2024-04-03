00b0a6df4a7fb7052f14240bbfffa0328cb53dd2
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-03 07:48:35 -0400

    Add Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/site/Header";
 import Footer from "@/components/site/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header></Header>
         <main>{children}</main>
         <Footer></Footer>
       </body>
