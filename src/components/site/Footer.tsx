3c4245d6a970906fcaf8896806824a0accc17f8f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-04 14:34:04 -0400

    Render contacts and social icons dynamically

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 import TextInput from "@/components/app/InputText";
 
+const CONTACTS = [
+  "Luis Escala",
+  "Piedras 623",
+  "Piso 2, depto 4",
+  "C1070AAM, Capital Federal",
+];
+
+const SOCIAL_ICONS = [
+  { src: "Facebook.svg", alt: "Facebook Logo" },
+  { src: "Instagram.svg", alt: "Instagram Logo" },
+  { src: "TikTok.svg", alt: "TikTok Logo" },
+];
+
 export default function Footer() {
   return (
     <footer>
@@ -9,33 +22,23 @@ export default function Footer() {
         <div>
           <h2 className="text-2xl">Where are we</h2>
           <ul className="mt-5">
-            <li>Luis Escala</li>
-            <li>Piedras 623</li>
-            <li>Piso 2, depto 4</li>
-            <li>C1070AAM, Capital Federal</li>
+            {CONTACTS.map((contact, index) => (
+              <li key={`contact-${index}`}>{contact}</li>
+            ))}
           </ul>
         </div>
         <div>
           <h2 className="text-2xl">Connect with Us</h2>
           <div className="flex gap-1 mt-5">
-            <Image
-              src="Facebook.svg"
-              alt="Facebook logo"
-              width={42}
-              height={42}
-            ></Image>
-            <Image
-              src="Instagram.svg"
-              alt="Instagram logo"
-              width={42}
-              height={42}
-            ></Image>
-            <Image
-              src="TikTok.svg"
-              alt="TikTok logo"
-              width={42}
-              height={42}
-            ></Image>
+            {SOCIAL_ICONS.map((icon, index) => (
+              <Image
+                key={`social-icon-${index}`}
+                src={icon.src}
+                alt={icon.alt}
+                width={42}
+                height={42}
+              ></Image>
+            ))}
           </div>
         </div>
         <div>
