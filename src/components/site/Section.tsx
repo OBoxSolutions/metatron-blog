5fc63f615e52ba89d1faa277492543dc86057272
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-08 11:49:09 -0400

    Increase spacing

--- src/components/site/Section.tsx
+++ src/components/site/Section.tsx
@@ -6,7 +6,7 @@ type SectionProps = {
 
 const Section = (props: SectionProps) => {
   return (
-    <section className={`my-5 ${props.className}`}>
+    <section className={`my-20 ${props.className}`}>
       {props?.title && <h2 className="text-5xl">{props.title}</h2>}
       {props.children}
     </section>
