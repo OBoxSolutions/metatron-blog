9adb67915e9e58bb3c6c43aee6b119cb1957ce50
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-08 11:31:29 -0400

    Make basic index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
+import { useMemo } from "react";
+
 import Image from "next/image";
 
+import Aside from "@/components/site/Aside";
+import Section from "@/components/site/Section";
+import Card from "@/components/app/Card";
+import CardBody from "@/components/app/CardBody";
+
+import { Post } from "@/types/Post";
+
+import posts from "./posts";
+
+function useFeaturedPosts(posts: Post[]): [Post | undefined, Post[]] {
+  return useMemo(() => {
+    const featuredPosts = posts.filter((post) => post.isFeatured);
+
+    return [featuredPosts.shift(), featuredPosts];
+  }, [posts]);
+}
+
+function useLatestPosts(posts: Post[]): [Post | undefined, Post[]] {
+  return useMemo(() => {
+    const latestPosts = posts.sort(
+      (a, b) => +new Date(a.date) - +new Date(b.date),
+    );
+
+    return [latestPosts.shift(), latestPosts];
+  }, [posts]);
+}
+
+function useOtherPosts(
+  posts: Post[],
+  featuredPosts: Post[],
+  latestPosts: Post[],
+) {
+  return posts.filter(
+    (post) => featuredPosts.includes(post) || latestPosts.includes(post),
+  );
+}
+
 export default function Home() {
+  const [firstFeaturedPost, featuredPosts] = useFeaturedPosts(posts);
+  const [firstLatestPost, latestPosts] = useLatestPosts(posts);
+  const otherPosts = useOtherPosts(posts, featuredPosts, latestPosts);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div className="max-w-screen-xl mx-auto">
+      {firstFeaturedPost && (
+        <Section>
+          <div className="md:grid grid-cols-6 gap-16">
+            <article className="col-span-4">
+              <Image
+                className="w-full"
+                src={firstFeaturedPost.image}
+                alt={firstFeaturedPost.description}
+                width={500}
+                height={400}
+              ></Image>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Blanditiis, veritatis ex? Tempora nisi corrupti, ut repudiandae
+                molestiae accusantium, nam error amet cum aliquam repellendus
+                ratione iste recusandae esse voluptas a?
+              </p>
+            </article>
+            <Aside className="col-span-2" posts={featuredPosts}></Aside>
+          </div>
+        </Section>
+      )}
+
+      {firstLatestPost && (
+        <Section title="Latest">
+          <div className="md:grid grid-cols-6 gap-16">
+            <article className="col-span-4">
+              <Image
+                className="w-full"
+                src={firstLatestPost.image}
+                alt={firstLatestPost.description}
+                width={500}
+                height={400}
+              ></Image>
+              <p>{firstLatestPost.description}</p>
+            </article>
+            <Aside className="col-span-2" posts={latestPosts}></Aside>
+          </div>
+        </Section>
+      )}
+
+      <Section className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {otherPosts.map((post) => (
+          <Card key={`index-post-${post.id}`}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              className="w-full"
+              src={post.image}
+              alt={post.description}
+              width={355}
+              height={240}
+            ></Image>
+            <CardBody>{post.description}</CardBody>
+          </Card>
+        ))}
+      </Section>
+    </div>
   );
 }
