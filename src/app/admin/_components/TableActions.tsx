fb73d2ef7c1331ba3645a30f3c67a08c428547fd
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-11 16:28:18 -0400

    Customize styles

--- src/app/admin/_components/TableActions.tsx
+++ src/app/admin/_components/TableActions.tsx
@@ -3,11 +3,11 @@ import { mdiPencil, mdiTrashCan } from "@mdi/js";
 
 export default function TableActions() {
   return (
-    <div className="flex gap-3">
-      <button>
+    <div className="flex gap-1">
+      <button className="bg-accent rounded-full p-2">
         <Icon path={mdiPencil} size={1}></Icon>
       </button>
-      <button>
+      <button className="bg-accent rounded-full p-2">
         <Icon path={mdiTrashCan} size={1}></Icon>
       </button>
     </div>
