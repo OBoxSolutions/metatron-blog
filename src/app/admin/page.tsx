68f5a97fda8719541f9eb034aba24a6f763ab2cd
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-11 11:47:46 -0400

    Change Admin content

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,13 @@
+import Card from "@/components/Card";
+import CardBody from "@/components/CardBody";
+
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <section className="max-w-6xl mx-auto mt-10">
+      <h2 className="text-3xl">Dashboard</h2>
+      <Card>
+        <CardBody>Some body</CardBody>
+      </Card>
+    </section>
+  );
 }
