7d34d0b7c8a30c06df93d07af307124166834f59
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-11 15:31:52 -0400

    Make basic posts page

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import Card from "@/components/Card";
 import CardBody from "@/components/CardBody";
+
 import Section from "../_components/Section";
+
+import DataTable from "../_components/DataTable";
+
+import { Post } from "@/types/Post";
+
+import posts from "@/app/_posts";
+
+const columns = [
+  {
+    name: "Title",
+    selector: (row: Post) => row.name,
+  },
+  {
+    name: "Year",
+    selector: (row: Post) => row.date,
+  },
+];
 
 export default function Posts() {
   return (
     <Section title="Posts">
       <Card>
-        <CardBody>Some body</CardBody>
+        <CardBody>
+          <DataTable columns={columns} data={posts} />
+        </CardBody>
       </Card>
     </Section>
   );
