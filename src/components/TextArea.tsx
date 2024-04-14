1b9f534366e5bd7f31086b1db1c82fb17e4e4da1
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-14 10:59:14 -0400

    Make basic TextArea component

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,15 @@
+import { InputProps } from "./Input";
+
+export default function TextArea(props: InputProps) {
+  return (
+    <div className="flex flex-col">
+      {props.label && <label htmlFor={props.label}>{props.label}</label>}
+      <textarea
+        id={props.label}
+        required={props.required}
+        v-model="text"
+        className="p-1 rounded outline-none mt-0 shadow shadow--press"
+      />
+    </div>
+  );
+}
