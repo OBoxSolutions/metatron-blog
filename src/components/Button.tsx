de1b45afd2cba51a3ae6540edef943cfc374baaa
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-14 12:07:49 -0400

    Add link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,33 @@
 import Icon from "@mdi/react";
+import Link from "next/link";
+import { ElementType } from "react";
 
 type ButtonProps = {
   icon?: string;
   iconSize?: number;
   children?: React.ReactNode;
   className?: string;
+  link?: string;
 };
 
 export default function Button(props: ButtonProps) {
   let buttonClasses = "";
+
+  const Parent: ElementType = props.link ? Link : "button";
+
+  const parentProps = props.link ? { href: props.link } : {};
 
   if (props.icon) {
     buttonClasses += "p-4 rounded-full bg-accent";
   }
 
   return (
-    <button className={`${buttonClasses} ${props.className}`}>
+    <Parent
+      className={`${buttonClasses} ${props.className}` || ""}
+      {...parentProps}
+    >
       {props.icon && <Icon path={props.icon} size={props.iconSize}></Icon>}
       {props.children}
-    </button>
+    </Parent>
   );
 }
