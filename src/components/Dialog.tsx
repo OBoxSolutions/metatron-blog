b96ddc5692af25d7b1165a475ae83a80542f6285
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-14 08:23:35 -0400

    Make basic Dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+
+type DialogProps = {
+  dialog: boolean;
+  closeDialog: (dialog: boolean) => void;
+  width?: string;
+  children?: React.ReactNode;
+};
+
+export default function Dialog({
+  dialog,
+  closeDialog,
+  width = "600px",
+  children,
+}: DialogProps) {
+  const dialogContentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleEvent(event: MouseEvent) {
+      if (!dialogContentRef?.current?.contains(event.target as Node)) {
+        closeDialog(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleEvent);
+
+    return () => {
+      document.removeEventListener("mousedown", handleEvent);
+    };
+  }, [dialog, closeDialog, dialogContentRef]);
+
+  if (!dialog) return null;
+
+  return (
+    <div className={`fixed inset-0 flex`}>
+      <div
+        className="h-fit w-fit m-auto z-20"
+        style={{ width }}
+        ref={dialogContentRef}
+      >
+        {children}
+      </div>
+      <div className="fixed inset-0 bg-gray-800 opacity-75 -z-10"></div>
+    </div>
+  );
+}
