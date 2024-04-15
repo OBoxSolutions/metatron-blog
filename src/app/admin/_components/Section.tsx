7d5e02be02f7b13ac240760cb4569850b7e6d2af
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-15 17:10:46 -0400

    Add width prop

--- src/app/admin/_components/Section.tsx
+++ src/app/admin/_components/Section.tsx
@@ -1,11 +1,12 @@
 type SectionProps = {
   title?: string;
   children?: React.ReactNode;
+  width?: string;
 };
 
 export default function Section(props: SectionProps) {
   return (
-    <section className="max-w-6xl mx-auto mt-10">
+    <section className={` max-w-6xl mx-auto mt-10 ${props.width}`}>
       {props.title && <h2 className="text-3xl mb-2">Posts</h2>}
       {props.children}
     </section>
