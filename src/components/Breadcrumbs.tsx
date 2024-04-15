338fe0f15a473799218e7877fdd8df81e169739e
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-15 17:04:39 -0400

    Make basic breadcrumbs component

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+type BreadcrumbsProps = {
+  breadcrumbs: Breadcrumb[];
+};
+
+type Breadcrumb = {
+  href: string;
+  text: string;
+};
+
+export default function Breadcrumbs(props: BreadcrumbsProps) {
+  const lastIndex = props.breadcrumbs.length - 1;
+
+  return (
+    <div className="flex">
+      {props.breadcrumbs.map((breadcrumb, index) => (
+        <div key={`breadcrumb-${breadcrumb.href}`} className="flex gag-10">
+          <Link
+            href={breadcrumb.href}
+            className={`${index !== lastIndex ? "text-accent" : "text-gray-400"}`}
+          >
+            {breadcrumb.text}
+          </Link>
+          {index !== lastIndex && <div>/</div>}
+        </div>
+      ))}
+    </div>
+  );
+}
