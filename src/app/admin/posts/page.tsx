f78b80f32761d07d10d3a9eab052dbfbbb55ed60
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-15 11:43:59 -0400

    Add logic to delete

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import { deleteDoc, doc } from "firebase/firestore";
 import { mdiPlus } from "@mdi/js";
 
 import Card from "@/components/Card";
@@ -7,12 +9,13 @@ import CardBody from "@/components/CardBody";
 import Button from "@/components/Button";
 
 import Section from "../_components/Section";
-
 import DataTable from "../_components/DataTable";
 
 import { Post } from "@/types/Post";
 
 import posts from "@/app/_posts";
+
+import { db } from "@/utils/firebase";
 
 const columns = [
   {
@@ -34,11 +37,27 @@ const columns = [
 ];
 
 export default function Posts() {
+  const [selectedRows, setSelectedRows] = useState<Post[]>([]);
+
+  const destroy = () => {
+    if (!selectedRows[0]) return;
+
+    const docRef = doc(db, "posts", String(selectedRows[0].id));
+    deleteDoc(docRef);
+  };
+
   return (
     <Section title="Posts">
       <Card>
         <CardBody>
-          <DataTable columns={columns} data={posts} />
+          <DataTable
+            columns={columns}
+            data={posts}
+            onSelectedRowsChange={({ selectedRows }) =>
+              setSelectedRows(selectedRows)
+            }
+            onDestroy={destroy}
+          />
         </CardBody>
       </Card>
       <Button
