e8821415739efd09973e5422ce4047d66f3ce6be
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-15 11:43:26 -0400

    Add props

--- src/app/admin/_components/TableActions.tsx
+++ src/app/admin/_components/TableActions.tsx
@@ -1,13 +1,18 @@
 import Icon from "@mdi/react";
 import { mdiPencil, mdiTrashCan } from "@mdi/js";
 
-export default function TableActions() {
+export type TableActionsProps = {
+  onDestroy?: () => void;
+  onUpdate?: () => void;
+};
+
+export default function TableActions(props: TableActionsProps) {
   return (
     <div className="flex gap-1">
-      <button className="bg-accent rounded-full p-2">
+      <button className="bg-accent rounded-full p-2" onClick={props.onUpdate}>
         <Icon path={mdiPencil} size={1}></Icon>
       </button>
-      <button className="bg-accent rounded-full p-2">
+      <button className="bg-accent rounded-full p-2" onClick={props.onDestroy}>
         <Icon path={mdiTrashCan} size={1}></Icon>
       </button>
     </div>
