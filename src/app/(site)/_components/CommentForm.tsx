9b5d06a7be2204d37f0507b14fddd9b69ccc9862
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-16 16:10:27 -0400

    Outsource submit

--- src/app/(site)/_components/CommentForm.tsx
+++ src/app/(site)/_components/CommentForm.tsx
@@ -1,34 +1,21 @@
 import { FormEvent } from "react";
 
-import { addDoc } from "firebase/firestore";
-
-import { postsCollection } from "@/utils/firebase";
-
 import Button from "@/components/Button";
 import TextArea from "@/components/TextArea";
 
 type CommentFormProps = {
   className: string;
+  onSubmit: (e: FormEvent) => void;
 };
 
 export default function CommentForm(props: CommentFormProps) {
-  const submit = (e: FormEvent) => {
-    const formData = new FormData(e.target as HTMLFormElement);
-
-    const data = {
-      text: formData.get("text") as string,
-    };
-
-    addDoc(postsCollection, data);
-  };
-
   return (
     <div className={props.className}>
       <h3 className="text-2xl">Comments</h3>
-      <form className="mt-2">
-        <TextArea required={true}></TextArea>
+      <form className="mt-2" onSubmit={props.onSubmit}>
+        <TextArea required={true} name="text"></TextArea>
         <div className="flex justify-end mt-4">
-          <Button onClick={submit}>Send</Button>
+          <Button>Send</Button>
         </div>
       </form>
     </div>
