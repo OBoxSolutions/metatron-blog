6e923a21613888386f213722e2242b13bfd2c174
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-15 20:59:42 -0400

    Make basic DeleteConfirmationDialog

---
+++ src/components/DialogDeleteConfirmation.tsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+import Dialog, { DialogProps } from "./Dialog";
+import Card from "./Card";
+import CardBody from "./CardBody";
+
+type DialogDeleteConfirmationProps = {
+  onClickAccept: () => void;
+} & DialogProps;
+
+export default function DialogDeleteConfirmation(
+  props: DialogDeleteConfirmationProps,
+) {
+  return (
+    <Dialog dialog={props.dialog} closeDialog={props.closeDialog} width="500px">
+      <Card>
+        <CardBody>
+          <p className="text-2xl">Delete?</p>
+          <p className="py-4">The selected element will be deleted</p>
+          <div className="flex justify-end gap-1">
+            <Button>Cancel</Button>
+            <Button onClick={props.onClickAccept}>Accept</Button>
+          </div>
+        </CardBody>
+      </Card>
+    </Dialog>
+  );
+}
