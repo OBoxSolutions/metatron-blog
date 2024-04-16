043dc4e8eca10e0554366b48f3cb178ebe5ce73d
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-16 08:27:41 -0400

    Use only one width class

--- src/app/admin/_components/Section.tsx
+++ src/app/admin/_components/Section.tsx
@@ -5,8 +5,10 @@ type SectionProps = {
 };
 
 export default function Section(props: SectionProps) {
+  const localWidth = props.width ?? "max-w-6xl";
+
   return (
-    <section className={` max-w-6xl mx-auto mt-10 ${props.width}`}>
+    <section className={`mx-auto mt-10 ${localWidth}`}>
       {props.title && <h2 className="text-3xl mb-2">Posts</h2>}
       {props.children}
     </section>
