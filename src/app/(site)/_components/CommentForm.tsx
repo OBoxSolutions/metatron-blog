91b58b46147effcb006c4fd4a8e4109a7d9966ce
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-16 20:24:27 -0400

    Remove h tag

--- src/app/(site)/_components/CommentForm.tsx
+++ src/app/(site)/_components/CommentForm.tsx
@@ -4,20 +4,17 @@ import Button from "@/components/Button";
 import TextArea from "@/components/TextArea";
 
 type CommentFormProps = {
-  className: string;
+  className?: string;
   onSubmit: (e: FormEvent) => void;
 };
 
 export default function CommentForm(props: CommentFormProps) {
   return (
-    <div className={props.className}>
-      <h3 className="text-2xl">Comments</h3>
-      <form className="mt-2" onSubmit={props.onSubmit}>
-        <TextArea required={true} name="text"></TextArea>
-        <div className="flex justify-end mt-4">
-          <Button>Send</Button>
-        </div>
-      </form>
-    </div>
+    <form className={props.className} onSubmit={props.onSubmit}>
+      <TextArea required={true} name="text"></TextArea>
+      <div className="flex justify-end mt-4">
+        <Button>Send</Button>
+      </div>
+    </form>
   );
 }
