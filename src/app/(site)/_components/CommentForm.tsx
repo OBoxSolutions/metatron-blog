48af3b793e8c589fd631ac7de2d620f0ab96652f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-16 20:49:46 -0400

    Add loading logic

--- src/app/(site)/_components/CommentForm.tsx
+++ src/app/(site)/_components/CommentForm.tsx
@@ -5,15 +5,16 @@ import TextArea from "@/components/TextArea";
 
 type CommentFormProps = {
   className?: string;
+  loading: boolean;
   onSubmit: (e: FormEvent) => void;
 };
 
 export default function CommentForm(props: CommentFormProps) {
   return (
     <form className={props.className} onSubmit={props.onSubmit}>
-      <TextArea required={true} name="text"></TextArea>
+      <TextArea required={true} name="text" disabled={props.loading}></TextArea>
       <div className="flex justify-end mt-4">
-        <Button>Send</Button>
+        <Button loading={props.loading}>Send</Button>
       </div>
     </form>
   );
