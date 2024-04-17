b652f3e1a2f257c536811b532a7af017527a7095
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-17 08:49:28 -0400

    Make Cancel button outlined

--- src/components/DialogDeleteConfirmation.tsx
+++ src/components/DialogDeleteConfirmation.tsx
@@ -18,7 +18,9 @@ export default function DialogDeleteConfirmation(
           <p className="text-2xl">Delete?</p>
           <p className="py-4">The selected element will be deleted</p>
           <div className="flex justify-end gap-1">
-            <Button onClick={props.onClickCancel}>Cancel</Button>
+            <Button onClick={props.onClickCancel} outline>
+              Cancel
+            </Button>
             <Button onClick={props.onClickAccept}>Accept</Button>
           </div>
         </CardBody>
