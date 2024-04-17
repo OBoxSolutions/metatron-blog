ab747fa816591865d3048ed1444bf2deaa253cfe
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-17 16:41:43 -0400

    Make users services

---
+++ src/services/users.ts
@@ -0,0 +1,46 @@
+import { User } from "@/types/User";
+import { db, usersCollection } from "@/utils/firebase";
+import {
+  DocumentReference,
+  addDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+
+export async function index(): Promise<User[]> {
+  const querySnapshot = await getDocs(usersCollection);
+
+  const localPosts = querySnapshot.docs.map((doc) => {
+    return { ...doc.data(), id: doc.id };
+  });
+
+  return localPosts as User[];
+}
+
+export async function show(id: string): Promise<User> {
+  const docRef = doc(db, "users", id);
+  const querySnapshot = await getDoc(docRef);
+  const localPost = { ...(querySnapshot.data() as User), id: querySnapshot.id };
+
+  return localPost;
+}
+
+export async function store(user: User): Promise<DocumentReference> {
+  const docRef = await addDoc(usersCollection, user);
+  return docRef;
+}
+
+export async function update(user: User): Promise<DocumentReference> {
+  const docRef = doc(db, "users", String(user.id));
+  await updateDoc(docRef, user);
+
+  return docRef;
+}
+
+export async function destroy(id: string) {
+  const docRef = doc(db, "users", id);
+  await deleteDoc(docRef);
+}
