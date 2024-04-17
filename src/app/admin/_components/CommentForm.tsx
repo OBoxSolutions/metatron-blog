aea5980b4a1d2071f56f91c99bc938437e5d2625
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-17 09:03:10 -0400

    Add logic to update comment

--- src/app/admin/_components/CommentForm.tsx
+++ src/app/admin/_components/CommentForm.tsx
@@ -1,5 +1,7 @@
 import Button from "@/components/Button";
-import InputText from "@/components/InputText";
+import Card from "@/components/Card";
+import CardBody from "@/components/CardBody";
+import TextArea from "@/components/TextArea";
 import { Comment } from "@/types/Comment";
 
 type CommentFormProps = {
@@ -10,11 +12,20 @@ export default function CommentForm(props: CommentFormProps) {
   const comment = { ...props.comment };
 
   return (
-    <form>
-      <InputText required={true}></InputText>
-      <div className="flex justify-end">
-        <Button onClick={(_) => props.onSubmit(comment)}>Submit</Button>
-      </div>
-    </form>
+    <Card>
+      <CardBody>
+        <p className="text-2xl mb-8">Update comment</p>
+        <form>
+          <TextArea
+            required={true}
+            defaultValue={comment.text}
+            label="Text"
+          ></TextArea>
+          <div className="flex justify-end mt-4">
+            <Button onClick={(_) => props.onSubmit(comment)}>Submit</Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
   );
 }
