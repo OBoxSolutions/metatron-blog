57c8991e064382de59a665d35c16c4aac66ee051
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-17 13:43:58 -0400

    Make basic posts services

---
+++ src/services/posts.ts
@@ -0,0 +1,37 @@
+import { Post } from "@/types/Post";
+import { db, postsCollection } from "@/utils/firebase";
+import {
+  DocumentReference,
+  addDoc,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+
+export async function index(): Promise<Post[]> {
+  const querySnapshot = await getDocs(postsCollection);
+
+  const localPosts = querySnapshot.docs.map((doc) => {
+    return { ...doc.data(), id: doc.id };
+  });
+
+  return localPosts as Post[];
+}
+
+export async function store(post: Post): Promise<DocumentReference> {
+  const docRef = await addDoc(postsCollection, post);
+  return docRef;
+}
+
+export async function update(post: Post): Promise<DocumentReference> {
+  const docRef = doc(db, "posts", String(post.id));
+  await updateDoc(docRef, post);
+
+  return docRef;
+}
+
+export async function destroy(id: string) {
+  const docRef = doc(db, "posts", id);
+  await deleteDoc(docRef);
+}
