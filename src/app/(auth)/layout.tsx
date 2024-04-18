e57818fa037caa60e60a549dbb29aa1379ce4a7a
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-18 10:55:17 -0400

    Add body and html

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
-import Link from "next/link";
+import { Hind_Siliguri } from "next/font/google";
 import "./globals.css";
+
+import Link from "next/link";
 import Icon from "@mdi/react";
 import { mdiArrowLeft } from "@mdi/js";
+
+const hindSiliguri = Hind_Siliguri({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-hind-siliguri",
+});
 
 export default function AuthLayout({
   children,
@@ -9,16 +18,20 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen flex flex-col justify-center bg-neutral items-center gap-10 text-white">
-      <Link
-        href="/"
-        className="text-accent text-xl fixed top-10 flex items-center gap-3"
-      >
-        <Icon path={mdiArrowLeft} size={1}></Icon>
-        Go Home
-      </Link>
-      <h1 className="text-white text-5xl">Metatron</h1>
-      <div className="max-w-md w-full mx-auto">{children}</div>
-    </div>
+    <html lang="en">
+      <body className={`${hindSiliguri.className}`}>
+        <div className="min-h-screen flex flex-col justify-center bg-neutral items-center gap-10 text-white">
+          <Link
+            href="/"
+            className="text-accent text-xl fixed top-10 flex items-center gap-3"
+          >
+            <Icon path={mdiArrowLeft} size={1}></Icon>
+            Go Home
+          </Link>
+          <h1 className="text-white text-5xl">Metatron</h1>
+          <div className="max-w-md w-full mx-auto">{children}</div>
+        </div>
+      </body>
+    </html>
   );
 }
