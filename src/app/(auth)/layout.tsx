60b0d0196579114422aaece8bceba85de25cb134
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-18 09:31:25 -0400

    Add Go Home link

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import "./globals.css";
+import Icon from "@mdi/react";
+import { mdiArrowLeft } from "@mdi/js";
 
 export default function AuthLayout({
   children,
@@ -7,6 +10,13 @@ export default function AuthLayout({
 }>) {
   return (
     <div className="min-h-screen flex flex-col justify-center bg-neutral items-center gap-10">
+      <Link
+        href="/"
+        className="text-accent text-xl fixed top-10 flex items-center gap-3"
+      >
+        <Icon path={mdiArrowLeft} size={1}></Icon>
+        Go Home
+      </Link>
       <h1 className="text-white text-5xl">Metatron</h1>
       <div className="max-w-md w-full mx-auto">{children}</div>
     </div>
