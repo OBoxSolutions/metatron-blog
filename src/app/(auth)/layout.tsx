63365f51a4ad4cfae511866c76d900d382365992
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-18 09:27:19 -0400

    Add header

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen flex flex-col justify-center bg-neutral">
+    <div className="min-h-screen flex flex-col justify-center bg-neutral items-center gap-10">
+      <h1 className="text-white text-5xl">Metatron</h1>
       <div className="max-w-md w-full mx-auto">{children}</div>
     </div>
   );
