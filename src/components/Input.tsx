bbb9d7b28f5e5ef7a076454671dcc120e1b22a68
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-19 17:23:27 -0400

    Make parent Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import { InputProps } from "./InputTypes";
+
+export default function Input<T>(props: InputProps<T>) {
+  return (
+    <div className="flex flex-col">
+      {props.label && (
+        <label htmlFor={props.label} className="text-white">
+          {props.label}
+        </label>
+      )}
+      {props.children}
+      {props?.error?.message && (
+        <p role="alert" className="text-red-400">
+          {props.error.message}
+        </p>
+      )}
+    </div>
+  );
+}
