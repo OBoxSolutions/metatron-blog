3684b59a80bd45db9eccb5285bcde04658870909
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-19 09:09:51 -0400

    Remove unnecessary import

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { mdiPlus } from "@mdi/js";
 
