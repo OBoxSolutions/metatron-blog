2cb636f5ab8cfd3656455aa7463e0bb5e0d2177e
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-23 08:50:04 -0400

    Translate to english

--- src/app/admin/_components/Nav.tsx
+++ src/app/admin/_components/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav(props: NavProps) {
       className={`flex bg-primary justify-end items-center px-5 gap-4 ${props.className}`}
     >
       <Link href="/" target="_blank">
-        Ver Sitio
+        See Site
       </Link>
       <div className="rounded-full p-3 bg-accent">
         <p className="w-6 h-6 text-center">JG</p>
