c7647d147be9428f207d37a55c9ac597405761ab
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-22 21:01:20 -0400

    Make basic layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,9 +5,29 @@ export default function Admin() {
   return (
     <section className="max-w-6xl mx-auto mt-10">
       <h2 className="text-3xl">Dashboard</h2>
-      <Card>
-        <CardBody>Some body</CardBody>
-      </Card>
+      <div className="grid grid-cols-4 gap-4">
+        <Card className="col-span-2">
+          <CardBody>Views</CardBody>
+        </Card>
+        <Card className="col-span-2">
+          <CardBody>Reads</CardBody>
+        </Card>
+        <Card className="col-span-2">
+          <CardBody>Registered Users</CardBody>
+        </Card>
+        <Card>
+          <CardBody>More popular categories</CardBody>
+        </Card>
+        <Card>
+          <CardBody>Most active writers</CardBody>
+        </Card>
+        <Card className="col-span-2">
+          <CardBody>Top read Posts</CardBody>
+        </Card>
+        <Card className="col-span-2">
+          <CardBody>New Posts</CardBody>
+        </Card>
+      </div>
     </section>
   );
 }
