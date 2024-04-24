d6b29fde7e9ff6903e383733dd8a4e8135133c6f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-24 08:56:05 -0400

    Add basic menu

--- src/app/admin/_components/Nav.tsx
+++ src/app/admin/_components/Nav.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 import Link from "next/link";
 
 import { mdiMenu } from "@mdi/js";
 
 import Button from "@/components/Button";
+import Menu from "@/components/Menu";
 
 type NavProps = {
   className?: string;
@@ -10,6 +13,9 @@ type NavProps = {
 };
 
 export default function Nav(props: NavProps) {
+  const [menu, setMenu] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+
   return (
     <nav
       className={`flex bg-primary items-center px-5 gap-4 ${props.className}`}
@@ -23,9 +29,21 @@ export default function Nav(props: NavProps) {
       <Link href="/" target="_blank">
         See Site
       </Link>
-      <div className="rounded-full p-3 bg-accent">
+      <div
+        className="rounded-full p-3 bg-accent cursor-pointer relative"
+        ref={(ref) => setAnchorEl(ref)}
+      >
         <p className="w-6 h-6 text-center">JG</p>
       </div>
+
+      {anchorEl && (
+        <Menu anchorEl={anchorEl} menu={menu} setMenu={setMenu}>
+          <ul className="flex flex-col gap-5">
+            <Link href="/admin/profile">Profile</Link>
+            <li>Logout</li>
+          </ul>
+        </Menu>
+      )}
     </nav>
   );
 }
