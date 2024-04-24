0fc08d171fc45a7a825e848559ab8d030c393906
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-24 05:31:58 -0400

    zustand, bcryptjs installs, auth.store implemented, func login, logout, register created

---
+++ src/stores/auth/auth.store.ts
@@ -0,0 +1,96 @@
+import { usersCollection } from "@/utils/firebase";
+import { findUserByEmail } from "@/utils/findUserByEmail";
+import { addDoc, getDocs, query, where } from "firebase/firestore";
+import { StateCreator, create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+import bcrypt from "bcryptjs";
+
+interface AuthState {
+  uid: string | null;
+  checking: boolean;
+  logged: boolean;
+  name: string | null;
+  email: string | null;
+}
+
+interface AuthActions {
+  login: (email: string, password: string) => Promise<void | null>;
+  register: (nombre: string, email: string, password: string) => Promise<void | null>;
+  logout: () => void;
+  // TODO: Implementar JWT
+  // checkToken: () => Promise<boolean>;
+}
+
+const authStoreApi: StateCreator<AuthState & AuthActions> = (set) => ({
+  uid: null,
+  checking: true,
+  logged: false,
+  name: null,
+  email: null,
+
+  login: async (email: string, password: string) => {
+    try {
+      const userData = await findUserByEmail(email);
+
+      if (!userData) return null;
+
+      const cryptPassword = userData.password;
+      const validPassword = bcrypt.compareSync(password, cryptPassword);
+
+      if (!validPassword) {
+        return null;
+      }
+      set((state) => ({
+        ...state,
+        uid: userData.id,
+        name: userData.name,
+        email,
+        checking: false,
+        logged: true,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  register: async (name: string, email: string, password: string) => {
+    try {
+    const userData = await findUserByEmail(email);
+
+    if (!userData) return null;
+
+      // encriptar contraña
+      const salt = bcrypt.genSaltSync();
+      password = bcrypt.hashSync(password, salt);
+
+      const docRef = await addDoc(usersCollection, {
+        name,
+        email,
+        password,
+      });
+      const uid = docRef.id;
+      set({ uid, name, email, checking: false, logged: true });
+    } catch (error) {
+      console.error("Error en firestore", error);
+    }
+  },
+
+  logout: () => {
+    set({
+      uid: null,
+      checking: true,
+      logged: false,
+      name: null,
+      email: null,
+    });
+  },
+});
+
+const useAuthStore = create<AuthState & AuthActions>()(
+  devtools(
+    persist(authStoreApi, {
+      name: "auth-storage",
+    })
+  )
+);
+export default useAuthStore;
