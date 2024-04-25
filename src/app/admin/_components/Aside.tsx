d86714100884c524d0037b274194383814535443
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-25 07:51:13 -0400

    Set height to screen

--- src/app/admin/_components/Aside.tsx
+++ src/app/admin/_components/Aside.tsx
@@ -39,14 +39,16 @@ const Aside = (props: AsideProps) => {
   const pathname = usePathname();
 
   return (
-    <aside className={`border-r border-gray-800 ${props.className}`}>
+    <aside className={`border-r border-gray-800 h-screen ${props.className}`}>
       <h3 className="pl-4 py-4 text-2xl">Metatron Blog</h3>
       <ul className="p-4 flex flex-col gap-2">
         {links.map((link) => {
           return (
             <li
               key={`link-${link.text}`}
-              className={` rounded flex items-center px-4 ${pathname === link.href ? "bg-accent" : ""}`}
+              className={` rounded flex items-center px-4 ${
+                pathname === link.href ? "bg-accent" : ""
+              }`}
             >
               <Icon path={link.icon} size={1}></Icon>
               <Link className="p-3 block w-full" href={link.href}>
