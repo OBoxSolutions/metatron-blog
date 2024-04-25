6559faa547e9ed5c6008644f2bac4ee8c1335208
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-25 16:00:34 -0400

    Add loading indicator

--- src/app/admin/loading.tsx
+++ src/app/admin/loading.tsx
@@ -1,5 +1,10 @@
+import LoadingIndicator from "@/components/LoadingIndicator";
 import Overlay from "@/components/Overlay";
 
 export default function Loading() {
-  return <Overlay className="absolute"></Overlay>;
+  return (
+    <Overlay className="absolute">
+      <LoadingIndicator iconSize={3} className="m-auto" />
+    </Overlay>
+  );
 }
