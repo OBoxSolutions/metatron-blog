efaee7116e93b2eba2d1d0841251fa3886aac7b6
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-27 09:18:04 -0400

    Bind onSubmit event

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,9 +2,14 @@ import { mdiMagnify } from "@mdi/js";
 import InputText from "./InputText";
 import Button from "./Button";
 
-export default function SearchBar({ className }: { className: string }) {
+type SearchBarProps = {
+  className?: string;
+  onSubmit?: (e: any) => void;
+};
+
+export default function SearchBar({ className, onSubmit }: SearchBarProps) {
   return (
-    <form className={`relative ${className} pt-0`}>
+    <form className={`relative ${className} pt-0`} onSubmit={onSubmit}>
       <InputText className="rounded-full py-3 pl-7"></InputText>
       <span className="absolute right-1 top-4">
         <Button icon={mdiMagnify} iconSize={1} iconPadding="p-3"></Button>
