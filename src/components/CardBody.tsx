9520a929c976d6235e47ff42411b824222998934
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-27 10:54:40 -0400

    Pass down className

--- src/components/CardBody.tsx
+++ src/components/CardBody.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from "react";
 
-export default function CardBody({ children }: { children: ReactNode }) {
-  return <div className="p-4">{children}</div>;
+export type CardBodyProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export default function CardBody({ children, className }: CardBodyProps) {
+  return <div className={`p-4 ${className}`}>{children}</div>;
 }
