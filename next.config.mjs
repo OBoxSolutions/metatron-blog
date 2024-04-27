600a14548d44ba1481b426b5edd4656d509b2585
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-27 09:37:57 -0400

    logo fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
     images: {
         domains: ["images.unsplash.com", "unsplash.com","firebasestorage.googleapis.com"],
     },
