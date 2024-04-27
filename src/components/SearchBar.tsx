226b6405e1d1ac3a94b69a2f5303589178b8b7e5
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-27 08:36:30 -0400

    Add padding to the left

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 export default function SearchBar({ className }: { className: string }) {
   return (
     <form className={`relative ${className} pt-0`}>
-      <InputText className="rounded-full py-3"></InputText>
+      <InputText className="rounded-full py-3 pl-7"></InputText>
       <span className="absolute right-1 top-4">
         <Button icon={mdiMagnify} iconSize={1} iconPadding="p-3"></Button>
       </span>
