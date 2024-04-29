22f645b2414557b22a88f60705426fd6bec6d189
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-29 14:44:22 -0400

    Add register name

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -46,6 +46,7 @@ function RegisterForm() {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                 message: "Invalid email address",
               }}
+              registerName="email"
               register={register}
               error={errors.email}
             ></InputText>
