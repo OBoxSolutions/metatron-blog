6f75885a851eec3c3acb615d20dec178280b467b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-30 09:12:00 -0400

    Register elements

--- src/app/admin/_components/dashboard-charts/ReadsLine.tsx
+++ src/app/admin/_components/dashboard-charts/ReadsLine.tsx
@@ -11,6 +11,16 @@ import {
   Legend,
 } from "chart.js";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
 import { options } from "@/utils/options";
 
 import Card from "@/components/Card";
