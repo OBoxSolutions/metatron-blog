8afbb3b0c80feef4e2ff69a7d880b92e833a9efa
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-12-21 17:01:27 -0500

    Create firebaseAdmin plugin

---
+++ src/utils/firebaseAdmin.ts
@@ -0,0 +1,26 @@
+import admin, { ServiceAccount } from "firebase-admin";
+
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env?.FIREBASE_PRIVATE_KEY,
+};
+
+const databaseURL = "https://metatron-blog.firebaseio.com";
+
+export function getFirebaseAdmin() {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL,
+    });
+  }
+
+  return admin;
+}
+
+const firebaseAdmin = getFirebaseAdmin();
+
+const db = firebaseAdmin.firestore();
+
+export { firebaseAdmin, db };
